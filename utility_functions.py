3f0bfd74b636c1c331e181d25cdc402482747a64
Zachary Clement <clementzach@icloud.com>
2021-12-31 15:46:21 -0700

    Functions now work in notebooks

--- utility_functions.py
+++ utility_functions.py
@@ -2,11 +2,12 @@ from sklearn.utils import resample
 
 import pandas as pd
 
-def hello_func(char):
-    print('hello')
-    return('hello')
 
 def rebalance_classes(input_df):
+
+    from sklearn.utils import resample
+
+    import pandas as pd 
     class_1 = input_df.loc[input_df.fetal_health == '1']
     class_2 = input_df.loc[input_df.fetal_health == '2']
     class_3 = input_df.loc[input_df.fetal_health == '3']
@@ -18,6 +19,10 @@ def rebalance_classes(input_df):
     return(output_df)
 
 def accuracy_by_class(predicted, actual):
+    from sklearn.metrics import f1_score
+
+    import numpy as np
+    import pandas as pd
     out_vector = []
     out_vector.append(np.mean(predicted == actual))
     for i in range(1, 4):
@@ -25,3 +30,16 @@ def accuracy_by_class(predicted, actual):
     out_vector.append(np.mean(out_vector[1:4]))
     out_vector.append(f1_score(y_true = actual, y_pred = predicted, average = "macro"))
     return(out_vector)
+
+
+def print_metrics(final_accuracy):
+    from os import linesep
+    print_str = "Overall accuracy: {}".format(final_accuracy[0]) + linesep
+
+    for i in range(1,4):
+        print_str = print_str + "Accuracy for class {}: {}".format(i, final_accuracy[i]) + linesep
+    
+    print_str = print_str + "Weighted Accuracy: {}".format(final_accuracy[4]) + linesep
+
+    print_str = print_str + "Macro F1 score: {}".format(final_accuracy[5]) + linesep
+    return(print_str)
